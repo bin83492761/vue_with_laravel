a71a8ab514041ff840c1cfcd1fd69dc02cd0339c
bin83492761 <140990757+bin83492761@users.noreply.github.com>
2023-09-19 13:16:44 +0800

    可以穩定跳頁

---
+++ resources/js/Pages/Backend/Product/Index.vue
@@ -0,0 +1,26 @@
+<script>
+import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
+import { Head } from '@inertiajs/vue3';
+
+export default {
+  components: {
+    AuthenticatedLayout,
+    Head,
+  },
+};
+</script>
+
+<template>
+  <Head title="product" />
+  <AuthenticatedLayout>
+    <template #header>
+      <h2 class="font-semibold text-xl text-gray-800 leading-tight">Product</h2>
+    </template>
+    <section id="product">
+    </section>
+  </AuthenticatedLayout>
+</template>
+
+<style lang="scss" scoped>
+#product {}
+</style>
